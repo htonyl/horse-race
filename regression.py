cd51e5407e0bc95eb4b370f3fdaced1aa96d0a5c
carstencww <carstencww@gmail.com>
2018-04-28 14:27:47 +0800

    upload result

--- regression.py
+++ regression.py
@@ -3,8 +3,8 @@ from sklearn.svm import SVR
 from sklearn.ensemble import GradientBoostingRegressor
 from sklearn.metrics import mean_squared_error
 from sklearn.preprocessing import StandardScaler
+import ctypes
 import numpy as np
-
 def validate(pred_result , true_labels, df):
 	rmse = np.sqrt(mean_squared_error(pred_result, true_labels))
 	aggreg  = df.join(pd.DataFrame({'pred_time': pred_result}))
@@ -31,6 +31,17 @@ def extract(df):
 		labels[i][0] = float(min)*60*60 + float(sec) * 60 + float(msec)
 	labels = np.reshape(labels,-1)
 	return features, labels
+
+
+mkl_rt = ctypes.CDLL('libmkl_rt.so')
+mkl_get_max_threads = mkl_rt.mkl_get_max_threads
+print(mkl_get_max_threads)
+def mkl_set_num_threads(cores):
+	mkl_rt.mkl_set_num_threads(ctypes.byref(ctypes.c_int(cores)))
+
+mkl_set_num_threads(4)
+print( mkl_get_max_threads())
+
 # Read
 fname = './training.csv'
 print('Reading dataframe from ', fname)
@@ -56,6 +67,26 @@ norm_train_labels = label_scaler.fit_transform(train_labels.reshape(-1,1)).resha
 
 
 #4.1.1.
+#epsilon=0.2
+svr_param_space = {"C":[0.01, 0.05,0.2,0.5,1,2],
+		"epsilon":[0.01,0.05,0.15, 0.2]
+			}
+log = []
+for c in svr_param_space["C"]:
+	for eps in svr_param_space["epsilon"]:
+		print("\n")
+		norm_svr_model = SVR(cache_size=6000,kernel='poly',C=c, epsilon=eps)
+		norm_svr_model.fit(norm_train_feats,norm_train_labels)
+		norm_svr_pred = norm_svr_model.predict(norm_test_feats)
+		unnorm_svr_pred = label_scaler.inverse_transform(norm_svr_pred)
+		norm_svr_stat =("norm_svr_model", ) + validate(unnorm_svr_pred, true_labels, test_data[['race_id', 'finishing_position']])
+		mess = "C "+str(c)+";epsilon "+str(eps)+str(norm_svr_stat)
+		print(mess)
+		log.append(mess)
+with open("./svm1.log","w") as logf:
+	logf.write("\n".join(log))
+
+'''
 svr_model = SVR(cache_size=2000,kernel='linear',C=1.0, epsilon=0.1)
 svr_model.fit(train_features,train_labels)
 svr_pred = svr_model.predict(test_features)
@@ -68,7 +99,7 @@ norm_svr_pred = norm_svr_model.predict(norm_test_feats)
 unnorm_svr_pred = label_scaler.inverse_transform(norm_svr_pred)
 norm_svr_stat =("norm_svr_model", ) + validate(unnorm_svr_pred, true_labels, test_data[['race_id', 'finishing_position']])
 print(norm_svr_stat)
-
+'''
 
 #4.1.2.
 """
