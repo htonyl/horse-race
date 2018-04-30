b10d33ab849dac23903b5bcee9d8a1d2b285ebb8
carstencww <carstencww@gmail.com>
2018-04-30 18:03:36 +0800

    finish 6.2

---
+++ plot/scatter_plot.py
@@ -0,0 +1,46 @@
+import matplotlib.pyplot as plt
+import pandas as pd
+import numpy as np
+
+fname = '../data/race-result-horse.csv'
+print('Reading dataframe from ', fname)
+print('\n')
+
+df = pd.read_csv(fname)
+cond = np.where([not (type(s) == type('') and s.isdigit()) for s in df['finishing_position']])
+df = df.drop(cond[0], axis=0)
+
+horses = df.groupby('horse_name')
+h_numwin = horses.apply(lambda v: len(v[v.finishing_position == "1"]))
+h_numwin.name = 'number_of_win'
+h_winrate = horses.apply(lambda v: float(len(v[v.finishing_position == "1"]))/len(v))
+h_winrate.name = 'win_rate'
+
+jocks = df.groupby('jockey')
+j_numwin = jocks.apply(lambda v: len(v[v.finishing_position == "1"]))
+j_numwin.name = 'number_of_win'
+j_winrate = jocks.apply(lambda v: float(len(v[v.finishing_position == "1"]))/len(v))
+j_winrate.name = 'win_rate'
+
+
+fig, ax = plt.subplots()
+ax.scatter(h_winrate,h_numwin,s=10)
+for i in range(len(h_winrate)):
+    if h_numwin.iloc[i] > 4 and h_winrate.iloc[i] > 0.5 :
+        ax.annotate(str(h_numwin.axes[0][i]) ,(h_winrate.iloc[i]+0.01,h_numwin.iloc[i]))
+        #print(h_numwin.axes[0][i])
+plt.xlabel("Win Rate")
+plt.ylabel("Number of wins")
+plt.title("Winning statistics of horses")
+plt.show()
+
+fig, ax = plt.subplots()
+ax.scatter(j_winrate,j_numwin,s=10)
+for i in range(len(j_winrate)):
+    if j_numwin.iloc[i] > 100 and j_winrate.iloc[i] > 0.2 :
+        ax.annotate(str(j_numwin.axes[0][i]) ,(j_winrate.iloc[i]+0.005,j_numwin.iloc[i]))
+        #print(j_numwin.axes[0][i])
+plt.xlabel("Win Rate")
+plt.ylabel("Number of wins")
+plt.title("Winning statistics of jockeys")
+plt.show()
